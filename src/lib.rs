77b27b034462eb3548a8dd9783b938c479f37624
Dmitriy <d.razumovskiy@kupibilet.ru>
2020-04-23 00:25:54 +0300

    work

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,3 @@
 mod bucket;
 mod map;
-
 pub use crate::map::HashMap;
-
-pub fn gen_assembly() {
-    let g = crossbeam::epoch::pin();
-    let m = HashMap::<u64, u64>::with_pow_buckets(2);
-    m.get(&0, &g);
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
