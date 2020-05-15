df89f133492976d8e7222ddae10b513f4e558675
Dmitriy <d.razumovskiy@kupibilet.ru>
2020-05-15 22:27:19 +0300

    add some docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! A concurrent hash table based on <a href="https://github.com/LPD-EPFL/CLHT">CLHT</a>.
+//!
+//! A hash table that supports full concurrency of retrievals and high expected concurrency for
+//! updates. This type is functionally very similar to `std::collections::HashMap`, and for the
+//! most part has a similar API. Even though all operations on the map are thread-safe and operate
+//! on shared references, retrieval operations do *not* entail locking, and there is *not* any
+//! support for locking the entire table in a way that prevents all access.
+
 mod bucket;
 mod map;
 
